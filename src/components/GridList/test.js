a24b9fcf8667c829cb91e016c1df22661f896f1c
Gustavo Gard <gusgard@gmail.com>
2017-11-19 16:57:45 -0300

    Feature/small fixes (#3)
    * update animation every time component is re-renderer
    
    * add more tests

--- src/components/GridList/test.js
+++ src/components/GridList/test.js
@@ -7,12 +7,39 @@ import GridList from './index';
 const logo = { uri: 'https://...' };
 const items = [{ id: 1, thumbnail: logo }, { id: 2, thumbnail: logo }];
 
-it('renders correctly', () => {
-  const wrapper = shallow(
-    <GridList
-      data={items}
-      renderItem={({ item }) => <Image source={item.thumbnail} />}
-    />,
-  );
-  expect(wrapper).toMatchSnapshot();
+describe('grid list', () => {
+  it('renders correctly', () => {
+    const wrapper = shallow(
+      <GridList
+        data={items}
+        renderItem={({ item }) => <Image source={item.thumbnail} />}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders showSeparator', () => {
+    const wrapper = shallow(
+      <GridList
+        showSeparator
+        data={items}
+        renderItem={({ item }) => <Image source={item.thumbnail} />}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('renders showAnimation', () => {
+    const wrapper = shallow(
+      <GridList
+        showSeparator
+        showAnimation
+        data={items}
+        renderItem={({ item, stagger }) => (
+          <Image source={item.thumbnail} onLoad={() => stagger.start()} />
+        )}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 });
