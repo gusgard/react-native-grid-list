0a2cd67fdb0162441f9f966dbbe36ba8a3df6055
Gustavo Gard <gusgard@gmail.com>
2017-11-19 15:58:37 -0300

    refactor name and add animation when loading (#2)

--- src/components/GridList/index.js
+++ src/components/GridList/index.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { ViewPropTypes, View, FlatList } from 'react-native';
+import { Animated, ViewPropTypes, View, FlatList } from 'react-native';
 
 import { width } from '../../themes';
 
@@ -11,12 +11,14 @@ const calcGridDimension = numColumns => ({
   width: width / numColumns,
 });
 
-export default class ListGrid extends PureComponent {
+export default class GridList extends PureComponent {
   static propTypes = {
     data: PropTypes.array.isRequired,
     numColumns: PropTypes.number.isRequired,
     renderItem: PropTypes.func.isRequired,
     showSeparator: PropTypes.bool,
+    showAnimation: PropTypes.bool,
+    animationDuration: PropTypes.number,
     itemStyle: ViewPropTypes.style,
   };
 
@@ -24,14 +26,38 @@ export default class ListGrid extends PureComponent {
     numColumns: 3,
     itemStyle: {},
     showSeparator: false,
+    showAnimation: false,
+    animationDuration: 500,
   };
 
+  componentWillMount() {
+    if (this.props.showAnimation) {
+      const { data, numColumns, animationDuration } = this.props;
+      this.animatedValue = [];
+
+      this.stagger = data.map((_, index) => {
+        this.animatedValue[index] = new Animated.Value(0);
+        return Animated.stagger(0, [
+          Animated.timing(this.animatedValue[index], {
+            toValue: 1,
+            duration: animationDuration * Math.ceil((index + 1) / numColumns),
+          }),
+        ]);
+      });
+    }
+  }
   itemDimension = calcGridDimension(this.props.numColumns);
 
   _keyExtractor = (item, index) => index;
 
   _renderItem = ({ item, index }) => {
-    const { showSeparator, renderItem, numColumns, itemStyle } = this.props;
+    const {
+      showAnimation,
+      showSeparator,
+      renderItem,
+      numColumns,
+      itemStyle,
+    } = this.props;
     let style = {};
     if (showSeparator) {
       const position = index % numColumns;
@@ -43,7 +69,19 @@ export default class ListGrid extends PureComponent {
         style = styles.imageCenter;
       }
     }
-    return (
+
+    return showAnimation ? (
+      <Animated.View
+        style={[
+          style,
+          this.itemDimension,
+          { opacity: this.animatedValue[index] },
+          itemStyle,
+        ]}
+      >
+        {renderItem({ item, index, stagger: this.stagger[index] })}
+      </Animated.View>
+    ) : (
       <View style={[style, this.itemDimension, itemStyle]}>
         {renderItem({ item, index })}
       </View>
